f6983ef55f0309590eeabc1f7fb381a92370d222
Сергей Иванов <sergejivanov@Mac-mini-Sergej.local>
2019-01-05 21:28:34 +0300

    Initial Commit

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Сергей Иванов on 05/01/2019.
+//  Copyright © 2019 topMob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
